8273d80678a650ab746396871692de40e53a525b
pXius <fan.engelbrecht@gmail.com>
2020-11-26 23:52:00 +0100

    Main Class Indentation  Update

--- src/main/java/se/pricer/rectest/parcers/Main.java
+++ src/main/java/se/pricer/rectest/parcers/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 1) {
             ParseHandler.parseFile(args[0]);
-        } else if (args.length == 2){
+            System.out.println("\nSorting available: Use 'asc' or 'desc' after filename.");
+        } else if (args.length == 2) {
             ParseHandler.parseFile(args[0], args[1]);
         } else {
             System.out.println("Invalid Arguments");
